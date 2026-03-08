88f3e80def8399000442f78b6feda5b3703325cc
Ryan Maber <ryan@ryanmaber.com>
2026-03-08 22:09:05 +0000

    Replace rand_seeder with built in seeding and hashing of the system id

--- src/helper/hardware.rs
+++ src/helper/hardware.rs
@@ -1,20 +1,17 @@
-use rand::{
-    distr::{Alphanumeric, SampleString},
-    rngs::StdRng,
-};
-use rand_seeder::Seeder;
+use base64::{Engine, engine::general_purpose::URL_SAFE};
+use sha2::{Digest, Sha256};
 
 /// Generates a hardware ID based on the provided system ID.
 ///
 /// The system ID can represent any unique identifier for the system, so long as it is predictable
 /// and consistent per device.
 ///
-/// The system ID is used as the seed for the hardware ID, so any changes to the system ID will
+/// The system ID is used as the hash for the hardware ID, so any changes to the system ID will
 /// result in a different hardware ID.
 pub fn generate_hardware_id(system_id: &str) -> String {
-    let mut rand: StdRng = Seeder::from(system_id).into_rng();
+    let hash = Sha256::digest(system_id.as_bytes());
 
-    Alphanumeric.sample_string(&mut rand, 32)
+    URL_SAFE.encode(hash)[..32].to_string()
 }
 
 #[cfg(test)]
@@ -24,7 +21,7 @@ mod tests {
         let system_id = "mock-system-id";
         let hardware_id = super::generate_hardware_id(system_id);
 
-        assert_eq!(hardware_id, "553MdASrIII8WL54vPBBr78lhXUH6WZu");
+        assert_eq!(hardware_id, "-LsI4fEAPwq_LcrcqompBfMDMQQltdlQ");
         assert_eq!(hardware_id.len(), 32);
     }
 }
